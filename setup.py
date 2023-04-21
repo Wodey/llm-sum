0bbc8c2dc0e4b30929a6108f4b4dfa5a12ef82d7
IvanneWest <ivannewest@IvanneWest.168.1.12>
2023-04-21 11:52:54 +0300

    initialization

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Collections of approaches to make LLM predict sum of 2 integer numbers correctly',
+    author='Wodey',
+    license='MIT',
+)
